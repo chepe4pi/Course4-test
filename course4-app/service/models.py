16d48e09083eb73355d8a6a6fd45d096586a1559
Alexey Kurylev <chepe4pi@gmail.com>
2022-11-21 00:47:58 +0300

    Day 6 - 2

--- course4-app/service/models.py
+++ course4-app/service/models.py
@@ -7,6 +7,19 @@ class Service(models.Model):
     name = models.CharField(max_length=100)
     full_price = models.PositiveSmallIntegerField()
 
+    def __init__(self, *args, **kwargs):
+        super(Service, self).__init__(*args, **kwargs)
+        self.__original_full_price = self.full_price
+
+    def save(self, *args, **kwargs):
+        from .tasks import set_price
+
+        super().save(*args, **kwargs)
+
+        if self.__original_full_price != self.full_price:
+            for subscription in self.subscription_set.all():
+                set_price.delay(subscription.id)
+
 
 class Plan(models.Model):
     PLAN_TYPES = (
@@ -19,6 +32,19 @@ class Plan(models.Model):
                                                         validators=[
                                                             MaxValueValidator(100),
                                                         ])
+
+    def __init__(self, *args, **kwargs):
+        super(Plan, self).__init__(*args, **kwargs)
+        self.__original_discount_percent = self.discount_percent
+
+    def save(self, *args, **kwargs):
+        from .tasks import set_price
+
+        super().save(*args, **kwargs)
+
+        if self.__original_discount_percent != self.discount_percent:
+            for subscription in self.subscription_set.all():
+                set_price.delay(subscription.id)
 
 
 class Subscription(models.Model):
