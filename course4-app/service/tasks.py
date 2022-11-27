16bc4559f7a76eeba1319a4f9f8a04c593b630e0
Alexey Kurylev <chepe4pi@gmail.com>
2022-11-27 20:39:25 +0300

    Day 7 - 1

--- course4-app/service/tasks.py
+++ course4-app/service/tasks.py
@@ -1,10 +1,14 @@
+import time
+
 from celery import shared_task
+from celery_singleton import Singleton
 
 from service.models import Subscription
 
 
-@shared_task
+@shared_task(base=Singleton)
 def set_price(subscription_id):
+    time.sleep(30)
     subscription = Subscription.objects.get(id=subscription_id)
     new_price = subscription.service.full_price - subscription.service.full_price * (subscription.plan.discount_percent / 100)
     subscription.price = new_price
